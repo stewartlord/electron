c585579c201b356e9eafa6913fa887642df91549
Samuel Attard <sattard@slack-corp.com>
2019-02-12 17:55:06 -0800

    chore: refactor app.ts into Typescript
    * Refactors app.dock into cpp
    * Removes app.launcher which has not existed for 3 years
    * Removes 2 deprecated APIs (that have been deprecated for more than one
    major)
    * Refactors deprecate.ts as well

--- typings/internal-electron.d.ts
+++ typings/internal-electron.d.ts
@@ -11,4 +11,22 @@ declare namespace Electron {
     setDesktopName(name: string): void;
     setAppPath(path: string | null): void;
   }
+
+  const deprecate: ElectronInternal.DeprecationUtil;
+}
+
+declare namespace ElectronInternal {
+  type DeprecationHandler = (message: string) => void;
+  interface DeprecationUtil {
+    setHandler(handler: DeprecationHandler): void;
+    getHandler(): DeprecationHandler | null;
+    warn(oldName: string, newName: string): void;
+    log(message: string): void;
+    function(fn: Function, newName: string): Function;
+    event(emitter: NodeJS.EventEmitter, oldName: string, newName: string): void;
+    removeProperty<T, K extends (keyof T & string)>(object: T, propertyName: K): T;
+    renameProperty<T, K extends (keyof T & string)>(object: T, oldName: string, newName: K): T;
+
+    promisify<T extends (...args: any) => any>(fn: T): T;
+  }
 }
